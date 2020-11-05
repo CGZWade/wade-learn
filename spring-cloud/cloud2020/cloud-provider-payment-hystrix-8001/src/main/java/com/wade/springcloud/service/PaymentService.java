8e2c7708af0c8a9a9ff08907d2337648989180a7
CGZ <354031409@qq.com>
2020-11-05 14:37:58 +0800

    SpringCloud-hystrix初步配置

---
+++ spring-cloud/cloud2020/cloud-provider-payment-hystrix-8001/src/main/java/com/wade/springcloud/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.wade.springcloud.service;
+
+public interface PaymentService {
+
+    public String paymentInfo_OK(Integer id);
+
+    public String paymentInfo_TimeOut(Integer id);
+
+}
