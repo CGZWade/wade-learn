ca493532d16f15ed7e1aa647a1207ed7356f562f
CGZWade <354031409@qq.com>
2021-01-25 23:34:49 +0800

    相应数据测试

--- vue-activiti/src/components/HelloWorld.vue
+++ vue-activiti/src/components/HelloWorld.vue
@@ -1,28 +1,26 @@
 <template>
-  <div>
-    <el-table>
-      <el-table-column prop="name" label="姓名"></el-table-column>
-    </el-table>
-  </div>
+    <div>
+        {{name}}
+    </div>
 </template>
 
 <script>
-    import { post, get, put, _delete } from '../plugins/axios'
     export default {
         name: 'HelloWorld',
         data() {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                msg: 'Welcome to Your Vue.js App',
+                name: ''
             }
         },
         mounted() {
-            this.query();
+            this.query()
         },
         methods: {
             async query() {
-                const that = this;
-                const res = await that.$axios.get('http://127.0.0.1:8002/hello/cgz');
-                console.log("----" + res.toString())
+                const that = this
+                const res = await that.$axios.get('http://127.0.0.1:8002/hello/cgz')
+                this.name =res.data.name
             },
         }
     }
@@ -30,21 +28,21 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+    h1, h2 {
+        font-weight: normal;
+    }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-  a {
-    color: #42b983;
-  }
+    a {
+        color: #42b983;
+    }
 </style>
