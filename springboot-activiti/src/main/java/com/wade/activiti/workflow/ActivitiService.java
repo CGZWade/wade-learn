98aa17c6b8367c1f80f333724bacb55eac1535d4
CGZWade <354031409@qq.com>
2021-05-18 23:10:28 +0800

    整合

---
+++ springboot-activiti/src/main/java/com/wade/activiti/workflow/ActivitiService.java
@@ -0,0 +1,13 @@
+package com.wade.activiti.workflow;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author CGZ
+ * @Date 2021/1/26
+ */
+@Service
+public class ActivitiService {
+    
+}
