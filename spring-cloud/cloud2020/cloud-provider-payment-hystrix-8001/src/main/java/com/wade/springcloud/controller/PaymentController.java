15d593412cede8445d3728626861bbcede469614
CGZWade <354031409@qq.com>
2021-06-29 21:44:52 +0800

    接口修改

--- spring-cloud/cloud2020/cloud-provider-payment-hystrix-8001/src/main/java/com/wade/springcloud/controller/PaymentController.java
+++ spring-cloud/cloud2020/cloud-provider-payment-hystrix-8001/src/main/java/com/wade/springcloud/controller/PaymentController.java
@@ -36,7 +36,7 @@ public class PaymentController {
     }
 
     // ******************************** 服务熔断 ********************************
-    @GetMapping("/payment/circuit/{id}")
+    @GetMapping("/circuit/{id}")
     public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
         String result = paymentService.paymentCircuitBreaker(id);
         log.info("*******result:" + result);
