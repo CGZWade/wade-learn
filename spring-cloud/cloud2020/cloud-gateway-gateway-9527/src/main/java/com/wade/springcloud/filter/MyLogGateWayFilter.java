7f7645b3197183636d35676d401a43608e99fc6f
CGZWade <354031409@qq.com>
2021-07-01 21:54:59 +0800

    新增 Gateway网关 全局过滤器

---
+++ spring-cloud/cloud2020/cloud-gateway-gateway-9527/src/main/java/com/wade/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.wade.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @Description 网关过滤器
+ * @Author CGZ
+ * @Date 2021/7/1
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("******************进入全局过滤器： " + new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null) {
+            log.info("*****************用户名为NULL，非法用户******************");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        // 加载过滤器顺序
+        return 0;
+    }
+
+}
