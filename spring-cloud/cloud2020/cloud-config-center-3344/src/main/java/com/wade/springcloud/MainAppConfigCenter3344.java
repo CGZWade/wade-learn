ab7cb1f4837238f33a5432485002e0548128d5ae
CGZWade <354031409@qq.com>
2021-07-01 23:06:53 +0800

    新增 服务配置中心 config

---
+++ spring-cloud/cloud2020/cloud-config-center-3344/src/main/java/com/wade/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,19 @@
+package com.wade.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author CGZ
+ * @Date 2021/7/1
+ */
+@SpringBootApplication
+public class MainAppConfigCenter3344 {
+
+    // TODO maven spring-cloud-config-server 导入失败
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class);
+    }
+
+}
