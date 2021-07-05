fffdeb301660e4b63fd77e651f868cfc6b81c5a4
CGZ <354031409@qq.com>
2021-07-05 14:17:47 +0800

    SpringCloud Stream + rabbitMQ 消息推送

---
+++ spring-cloud/cloud2020/cloud-stream-rabbitmq-consumer-8803/src/main/java/springcloud/MessageConsumerMain8803.java
@@ -0,0 +1,13 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageConsumerMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageConsumerMain8803.class);
+    }
+
+}
--- spring-cloud/cloud2020/cloud-stream-rabbitmq-consumer-8803/src/main/java/springcloud/controller/ReceiveMessageListenerController.java
+++ spring-cloud/cloud2020/cloud-stream-rabbitmq-consumer-8803/src/main/java/springcloud/controller/ReceiveMessageListenerController.java
@@ -1,4 +1,4 @@
-package com.wade.springcloud.controller;
+package springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
